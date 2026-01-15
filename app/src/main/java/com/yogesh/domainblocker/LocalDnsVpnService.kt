b3b9f2853f8c1ef6a71a6a4e4cbf4311d28945d4
yogeshdangii <yo9752548845@gmail.com>
2026-01-15 14:32:24 +0530

    defined structure of LocalDnsVpnService class

--- app/src/main/java/com/yogesh/domainblocker/LocalDnsVpnService.kt
+++ app/src/main/java/com/yogesh/domainblocker/LocalDnsVpnService.kt
@@ -1,4 +1,29 @@
 package com.yogesh.domainblocker
 
-class LocalDnsVpnService {
+import android.content.Intent
+import android.net.VpnService
+import android.util.Log
+
+class LocalDnsVpnService : VpnService() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("VpnService", "VPN Service Created!")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d("VpnService", "VPN Service Started! Waiting for TUN configuration.")
+
+        // i will set up TUN interface here
+
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        Log.d("VpnService", "VPN Service Destroyed!")
+
+        // Tun interface yaha khatam hoga
+        super.onDestroy()
+    }
 }
